411809b67a957a20a06d2684b01c3852d8b8a308
Greg Weber <greg@gregweber.info>
2016-08-10 10:29:31 -0700

    remove unused ecs variables in the top-level stack

--- main.tf
+++ main.tf
@@ -81,60 +81,6 @@ variable "bastion_ami_id" {
   default = ""
 }
 
-variable "ecs_cluster_name" {
-  description = "the name of the cluster, if not specified the variable name will be used"
-  default = ""
-}
-
-variable "ecs_instance_type" {
-  description = "the instance type to use for your default ecs cluster"
-  default     = "m4.large"
-}
-
-variable "ecs_min_size" {
-  description = "the minimum number of instances to use in the default ecs cluster"
-
-  // create 3 instances in our cluster by default
-  // 2 instances to run our service with high-availability
-  // 1 extra instance so we can deploy without port collisions
-  default = 3
-}
-
-variable "ecs_max_size" {
-  description = "the maximum number of instances to use in the default ecs cluster"
-  default     = 100
-}
-
-variable "ecs_desired_capacity" {
-  description = "the desired number of instances to use in the default ecs cluster"
-  default     = 3
-}
-
-variable "ecs_root_volume_size" {
-  description = "the size of the ecs instance root volume"
-  default     = 25
-}
-
-variable "ecs_docker_volume_size" {
-  description = "the size of the ecs instance docker volume"
-  default     = 25
-}
-
-variable "ecs_docker_auth_type" {
-  description = "The docker auth type, see https://godoc.org/github.com/aws/amazon-ecs-agent/agent/engine/dockerauth for the possible values"
-  default     = ""
-}
-
-variable "ecs_docker_auth_data" {
-  description = "A JSON object providing the docker auth data, see https://godoc.org/github.com/aws/amazon-ecs-agent/agent/engine/dockerauth for the supported formats"
-  default     = ""
-}
-
-variable "ecs_security_groups" {
-  description = "A comma separated list of security groups from which ingest traffic will be allowed on the ECS cluster, it defaults to allowing ingress traffic on port 22 and coming grom the ELBs"
-  default     = ""
-}
-
 module "defaults" {
   source = "./defaults"
   region = "${var.region}"
