aa6d8d5cf5fb07394aee40ce8d78d3b3a139ae9f
Greg Weber <greg@gregweber.info>
2016-08-15 22:00:18 -0700

    upgrade ecs-agent

--- packer/ecs/scripts/ecs.sh
+++ packer/ecs/scripts/ecs.sh
@@ -4,3 +4,7 @@ set -e
 systemctl daemon-reload
 systemctl enable ecs-agent.service
 systemctl enable ecs-logs.service
+
+# http://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-iam-roles.html#create_task_iam_policy_and_role
+sysctl -w net.ipv4.conf.all.route_localnet=1
+iptables -t nat -A PREROUTING -p tcp -d 169.254.170.2 --dport 80 -j DNAT --to-destination 127.0.0.1:51679
