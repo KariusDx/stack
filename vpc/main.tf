61b6dc48cbcfe9093af80482476be9c652df4c2f
Greg Roberts <greghroberts@gmail.com>
2016-09-25 15:31:48 -0700

    Added VPC Endpoint for S3

--- vpc/main.tf
+++ vpc/main.tf
@@ -152,6 +152,15 @@ resource "aws_route_table_association" "external" {
 }
 
 /**
+ * Endpoints for S3
+ */
+ resource "aws_vpc_endpoint" "private-s3" {
+    vpc_id = "${aws_vpc.main.id}"
+    service_name = "com.amazonaws.us-east-1.s3"
+    route_table_ids = ["${concat(aws_route_table.internal.*.id, aws_route_table.external.*.id)}"]
+}
+
+/**
  * Outputs
  */
 
